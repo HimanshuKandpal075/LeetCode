f750e26092ae880faf3ba6a468dff613b4ffffa6
Himanshu K <himanshukandpal075@gmail.com>
2026-08-23 01:12:47 +0530

    Time: 435 ms (14.71%), Space: 273.2 MB (11.59%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,21 +1,34 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
 
         for(int i=0;i<=s.length()-10;i++){
             String sub = s.substring(i, i+10);
-
-            map.put(sub, map.getOrDefault(sub, 0)+1);
-        }
-
-        List<String> ans = new ArrayList<>();
-
-        for(String key: map.keySet()){
-            if(map.get(key) > 1){
-                ans.add(key);
+            if(!seen.add(sub)){
+                repeated.add(sub);
             }
         }
-        return ans;
+        return new ArrayList<>(repeated);
     }
 }
+
+// public List<String> findRepeatedDnaSequences(String s) {
+//         HashMap<String, Integer> map = new HashMap<>();
+        
+
+//         for(int i=0;i<=s.length()-10;i++){
+//             String sub = s.substring(i, i+10);
+
+//             map.put(sub, map.getOrDefault(sub, 0)+1);
+//         }
+
+//         List<String> ans = new ArrayList<>();
+
+//         for(String key: map.keySet()){
+//             if(map.get(key) > 1){
+//                 ans.add(key);
+//             }
+//         }
+//         return ans;
+//     }
