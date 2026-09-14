7b0d40100156780793be3bef4bef412cdeadfcb9
Himanshu K <himanshukandpal075@gmail.com>
2026-09-14 18:54:53 +0530

    Time: 6 ms (99.57%), Space: 60.3 MB (51.19%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -3,30 +3,28 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        boolean[][] vis = new boolean[n][m];
-
         for(int i=0;i<n;i++){
-            if(grid[i][0] == 1 && !vis[i][0]){
-                dfs(grid, i, 0, vis);
+            if(grid[i][0] == 1 ){
+                helper(grid, i, 0);
             }
-            if(grid[i][m-1] == 1 && !vis[i][m-1]){
-                dfs(grid, i, m-1, vis);
+            if(grid[i][m-1] == 1  ){
+                helper(grid, i, m-1);
             }
         }
 
         for(int j=0;j<m;j++){
-            if(grid[0][j] == 1 && !vis[0][j]){
-                dfs(grid, 0, j, vis);
+            if(grid[0][j] == 1  ){
+                helper(grid, 0, j);
             }
-            if(grid[n-1][j] == 1 && !vis[n-1][j]){
-                dfs(grid, n-1, j, vis);
+            if(grid[n-1][j] == 1  ){
+                helper(grid, n-1, j);
             }
         }
 
         int count = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
+                if(grid[i][j] == 1){
                     count++;
                 }
             }
@@ -34,25 +32,21 @@ class Solution {
         return count;
     }
 
-    static void dfs(int[][] grid, int r, int c, boolean[][] vis){
+    static void helper(int[][] grid, int r, int c){
         int n = grid.length;
         int m = grid[0].length;
 
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, 1, -1};
-
-        vis[r][c] = true;
-
-        for(int i=0;i<4;i++){
-            int nR = r + dr[i];
-            int nC = c + dc[i];
-
-            if(nR >= 0 && nR < n &&
-                nC >= 0 && nC < m &&
-                grid[nR][nC] == 1 &&
-                !vis[nR][nC]){
-                    dfs(grid, nR, nC, vis);
-                }
+        if(r < 0 || r >= n ||
+            c < 0 || c >= m ||
+            grid[r][c] == 0 ){
+                return;
         }
+
+        grid[r][c] = 0;
+
+        helper(grid, r-1, c);
+        helper(grid, r+1, c);
+        helper(grid, r, c-1);
+        helper(grid, r, c+1);
     }
 }
